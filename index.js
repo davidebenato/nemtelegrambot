392204bea8becd10b2bc2446990da4a4f5908e57
Davide Benato <davide.benato@gmail.com>
2017-01-22 16:36:22 +0100

    finalizing bot

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const NEM     = require('./nodejs2nem/NEM.js');
-
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
 const StartController = Telegram.StartController
@@ -10,8 +9,7 @@ const TextCommand = Telegram.TextCommand
 var storage = require('node-persist');
 storage.initSync();
 
-//setting up the NEM api
-//var conf = { 'nis_address': 'go.nem.ninja'};
+// write the NIS address here (we picked one of the supernodes for the time being)
 var conf = { 'nis_address': '85.25.36.97'};
 
 var nem = new NEM(conf);
@@ -20,11 +18,12 @@ Array.prototype.contains = function(element){
     return this.indexOf(element) > -1;
 };
 
-const tg = new Telegram.Telegram('280681948:AAHSHszkiaa_4_u9vRekdE1PHKLPw5yedP0', {
+var telegram_key = '280681948:AAHSHszkiaa_4_u9vRekdE1PHKLPw5yedXX'
+const tg = new Telegram.Telegram(telegram_key, {
     workers: 1
 })
 
-function readAddress(address, old_wallet, cb){
+function readAddress(address, old_wallet, chat_id, cb){
     //formatting the address removing the -
     address = address.replace(/-/g,"")
     
@@ -33,12 +32,7 @@ function readAddress(address, old_wallet, cb){
     var query = '/account/get?address=' + address 
         nem.nisGet(query, null
         ,function(err) {
-            // error handler function
-
-            // output error here
             console.log(err);
-            // or call other functions 
-
         }
         ,function(res) {
 
@@ -54,68 +48,58 @@ function readAddress(address, old_wallet, cb){
             // output the  response here
             console.log(res);
             // or call other user defined code 
-            cb(old_wallet, res)
+            cb(old_wallet, res, chat_id)
             return;
         });
 }
 
 
-//TODO add a command to return list of registered wallets from a chat id
-
 tg.onMaster(() => {
+
     function checkAccounts(){
-        //console.log("callback")
-        //tg.api.sendMessage(302414140, 'pong')
         storage.forEach(function(key, value) {
 
         console.log(new Date().toString() + " chatId: " + key);
 
-        //var result = value.wallet
         var chat_id = parseInt(key)
-        for (var i = 0; i < value.length; i++) {
-            console.log('registered wallets -> ' + value[i].account.address)
 
-            //TODO check if something changed
+        console.log('registered wallet -> ' + value.account.address)
 
-            //query the wallets:
-            var old_wallet = value[i]
-            var wallet_key = value[i].account.address
-            readAddress(wallet_key, old_wallet, function (old_wallet, wallet) {
+        //query the wallet:
+        var old_wallet = value
+        var wallet_key = value.account.address
+        readAddress(wallet_key, old_wallet, chat_id, function (old_wallet, wallet, chat_id) {
 
-                if(wallet.error != undefined && wallet.error){
-                    console.log(wallet)
-                    $.sendMessage('Error: ' + wallet.error + ' -> ' + wallet.message)
-                    return
-                }
-                
-                if(wallet.error != undefined && wallet.error){
-                    console.log(wallet)
-                    return
-                }
+            if(wallet.error != undefined && wallet.error){
+                console.log(wallet)
+                $.sendMessage('Error: ' + wallet.error + ' -> ' + wallet.message)
+                return
+            }
+            
+            if(wallet.error != undefined && wallet.error){
+                console.log(wallet)
+                return
+            }
 
-                //TODO add more checks here if you want to be notified of different things
-                //TODO if things changed delete old wallet and store the new one
-                if(wallet.account.balance != old_wallet.account.balance){
-                    //tg.api.sendMessage(302414140, 'pong')
-                    tg.api.sendMessage(chat_id, 'Balance for [' + wallet_key + '] was [' + old_wallet.account.balance 
-                        + "] and now it's [" + wallet.account.balance+ "]")
+            //TODO add more checks here if you want to be notified of different things
+            if(wallet.account.balance != old_wallet.account.balance){
+                tg.api.sendMessage(chat_id, 'Balance for [' + wallet_key + '] was [' + old_wallet.account.balance 
+                    + "] and now it's [" + wallet.account.balance+ "]")
 
-                }
-                else{
-                    console.log("balance didn't change: " + wallet.account.balance)
-                    return;
-                }
-                
-                //storage.setItemSync($.chatId.toString(), result);
-                //$.sendMessage('Registered wallet: ' + wallet_key)
-            })
-        }
+                //saving results
+                console.log('saving wallet')
+                storage.setItemSync(chat_id.toString(), wallet);
+            }
+            else{
+                console.log("balance didn't change: " + wallet.account.balance)
+            }
+        })
 
-        });
+    })
+
     }
-
-    setInterval(checkAccounts, 1 * 30000); 
-
+    //checking every minunte
+    setInterval(checkAccounts, 1 * 60000); 
 })
 
 
@@ -126,27 +110,38 @@ tg.onMaster(() => {
 
 
 
-class PingController extends TelegramBaseController {
+class BalanceController extends TelegramBaseController {
    /**
    * @param {Scope} $
    */
-    pingHandler($) {
-        //storage.setItemSync($.chatId, "test");
-        //console.log("registered wallet: " + storage.getItemSync($.chatId)); // yourname
+    balanceHandler($) {
 
-        $.sendMessage('pong: chatId -> ' + $.chatId)
-        //$.sendMessage('wallets -> ' + storage.valuesWithKeyMatch($.chatId.toString()))
-
-        var result = storage.getItemSync($.chatId.toString()).wallet
-        //result.push("yetanotherwallet")
-        for (var i = 0; i < result.length; i++) {
-            $.sendMessage('registered wallets -> ' + result[i])
+        var result = storage.getItemSync($.chatId.toString())
+        if(result){
+            $.sendMessage('registered wallets -> ' + result.account.address + " balance: " + result.account.balance)
         }
     }
 
     get routes() {
         return {
-            'pingCommand': 'pingHandler'
+            'balanceCommand': 'balanceHandler'
+        }
+    }
+}
+
+class HelpController extends TelegramBaseController {
+   /**
+   * @param {Scope} $
+   */
+    helpHandler($) {
+        $.sendMessage('This bot registers a NEM wallet and notifies you when the balance changes')
+        $.sendMessage('/register to register a new wallet')
+        $.sendMessage('/balance to see your current balance')
+    }
+
+    get routes() {
+        return {
+            'helpCommand': 'helpHandler'
         }
     }
 }
@@ -172,106 +167,31 @@ class RegisterController extends TelegramBaseController {
                     var wallet_key = message.text.toString()
                     if(!wallet_key || wallet_key.length != 46){
                         $.sendMessage("wrong address, please provide a well formed address [NAYFRF-6C2DZK-KEQEE2-SNVBBD-G354SY-F4XHMY-JDFP]")
-                        //callback(false)
                         return
                     }
 
-                    //TODO check if we already have a value first
-                    var keys = storage.keys()
 
                     console.log("includes chatID: " + keys.contains($.chatId.toString()))
 
-                    //new client
-                    if(!keys.contains($.chatId.toString()))
-                    {
-                        readAddress(wallet_key, null, function (old, wallet) {
+                    //reading/saving wallet
+                    readAddress(wallet_key, null, null, function (old, wallet, chat_id) {
 
-                            if(wallet.error != undefined && wallet.error){
-                                console.log(wallet)
-                                $.sendMessage('Error: ' + wallet.error + ' -> ' + wallet.message)
-                                return
-                            }
-                            
-                            var result = [];
-                            if(wallet.error != undefined && wallet.error){
-                                console.log(wallet)
-                                return   
-                            }
-
-                            result.push(wallet)
-                            storage.setItemSync($.chatId.toString(), result);
-
-                            $.sendMessage('Registered wallet: ' + wallet_key)
-                            })
-                        return;
-                    }
-                    else
-                    {
-                        //existing client, adding address to eisting key
-                        var result = storage.getItemSync($.chatId.toString())
-                        
-                        for (var i = 0; i < result.length; i++) {
-                            if(result[i].account.address == wallet_key.replace(/-/g,"")){
-                                $.sendMessage('Already registered: ' + wallet_key)
-                                return;
-                            }
-                        }
-
-                        readAddress(wallet_key, null, function (old, wallet) {
-
-                            if(wallet.error != undefined && wallet.error){
-                                console.log(wallet)
-                                $.sendMessage('Error: ' + wallet.error + ' -> ' + wallet.message)
-                                return
-                            }
-                            
-                            if(wallet.error != undefined && wallet.error){
-                                console.log(wallet)                                
-                            }
-                            result.push(wallet)
-
-                            storage.setItemSync($.chatId.toString(), result);
-                            $.sendMessage('Registered wallet: ' + wallet_key)
-                            })
-
-
-                        
-                        /*
-                        for (var wallet in result) {
+                        if(wallet.error != undefined && wallet.error){
                             console.log(wallet)
-                            if(wallet.account.address == wallet_key.replace(/-/g,"")){
-                                $.sendMessage('Already registered: ' + wallet_key)
-                                return;
-                            }
+                            $.sendMessage('Error: ' + wallet.error + ' -> ' + wallet.message)
+                            return
                         }
-                        */
-                        //  if (goals.hasOwnProperty(name)) {
-                            //result.push({name: name, goals: goals[name]});
-                        //  }
+                        
+                        if(wallet.error != undefined && wallet.error){
+                            console.log(wallet)
+                            return   
+                        }
 
-                        return
-                    }
+                        storage.setItemSync($.chatId.toString(), wallet);
 
-                    //todo we also need to save the current amount of NEM that are in the wallet right now
-                    
-                    //var result = storage.getItemSync($.chatId.toString()).wallets
-                    //check that it's not duplicated first (insert only if we don't have it already)
-                    //result.push(message.text)
+                        $.sendMessage('Registered wallet: ' + wallet_key)
+                        })
 
-                    //var w = [message.text, "testwallet"]
-                    //w.push("anotherwallet")
-                    //storage.setItemSync($.chatId.toString(), {wallet: result});
-                    //storage.setItemSync($.chatId.toString(), values.wallet);
-
-                    /*
-
-                    var r = storage.getItemSync($.chatId.toString()).wallets
-                    for (var i = 0; i < r.length; i++) {
-                        console.log("wallet: " + r[i])
-                    }
-                    */
-                    //console.log("registered wallet: " + storage.getItemSync($.chatId.toString()).wallet.toString()); // yourname
-                    //console.log(storage.values())
                     callback(true, message.text) //you must pass the result also
                     return
                 }
@@ -279,10 +199,7 @@ class RegisterController extends TelegramBaseController {
         }
 
         $.runForm(form, (result) => {
-            //console.log(result)
-            //$.sendMessage('registered!')
         })
-        //$.sendMessage('registered!')
     }
 
     get routes() {
@@ -293,10 +210,9 @@ class RegisterController extends TelegramBaseController {
 }
 
 tg.router
-    //.when(new TextCommand('/start', 'startHandler'), new StartController())
-    .when(new TextCommand('/ping', 'pingCommand'), new PingController())
+    .when(new TextCommand('/help', 'helpCommand'), new HelpController())
+    .when(new TextCommand('/balance', 'balanceCommand'), new BalanceController())
     .when(new TextCommand('/register', 'registerCommand'),new RegisterController())
     .otherwise(new OtherwiseController())
-
 
 console.log("Starting bot!")
